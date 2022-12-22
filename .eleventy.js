7db8dd77e9f4281d2322730cc102bcf0e4cce7fb
devilbv <79283548+devilbv@users.noreply.github.com>
2022-12-23 00:21:16 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,7 @@ const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
     
-   eleventyConfig.addPassthroughCopy('./src/style.css');    
+   eleventyConfig.addPassthroughCopy('./src/style.css');      
    eleventyConfig.addPassthroughCopy('./src/assets');    
 
    eleventyConfig.addFilter("postDate", (dateObj) => {
